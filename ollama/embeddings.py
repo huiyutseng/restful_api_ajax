4ce189a116c0d06a4171de7e2c7c56c18e72d82f
Darren Yang <telunyang@gmail.com>
2026-06-10 00:16:42 +0800

    更新 ollama 範例

--- ollama/embeddings.py
+++ ollama/embeddings.py
@@ -7,17 +7,22 @@ import time
 
 async def embed_text():
     t1 = time.time()
+
     response = await AsyncClient(
         host='http://localhost:11434',
         timeout=600,
     ).embed(
         model='bge-m3',
-        input='The sky is blue because of Rayleigh scattering',
+        input='早安', # 你也可以放一句以上，例如 input=['早安', '早上好']，就會得到兩句話的嵌入向量
         keep_alive='1h',
     )
-    print(response.embeddings)
+
+    print(response.embeddings[0])
+
     t2 = time.time()
-    print(f'Response time: {t2 - t1:.2f} seconds')
+
+    print(f'執行時間: {t2 - t1:.2f} 秒')
 
 
-asyncio.run(embed_text())
+if __name__ == '__main__':
+    asyncio.run(embed_text())
